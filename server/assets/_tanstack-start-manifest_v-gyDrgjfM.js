6b377c51f37f7d8505d194e606fbb4f2621f79af
peddireddypadmachandana <peddireddypadmachandana@gmail.com>
2026-05-07 09:36:28 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-gyDrgjfM.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Sravya/cafe26/cafe_26/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/cafe_26/assets/index-DFk-3_Bc.js"] }, "/": { filePath: "C:/Users/Sravya/cafe26/cafe_26/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/cafe_26/assets/index-BB-QeQlg.js"] } }, clientEntry: "/cafe_26/assets/index-DFk-3_Bc.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-CA0IUF2Y.js";
-import "node:events";
-export {
-  w as default
-};
